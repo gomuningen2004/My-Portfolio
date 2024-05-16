58dc1d6ab1c2b92a66311e3d89099958bdce0801
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-05-16 21:58:33 +0530

    minor changes

--- hie.py
+++ hie.py
@@ -2,3 +2,10 @@
 is this visible?
 yep
 fine 
+ill delete this thing i
+g no itlet it be for now
+??
+I want to check something else too
+what?
+like I am using gitlens so if you are signed in with github then we can see who is changing what
+yes im signed in using github acc. in VS coed? yep
